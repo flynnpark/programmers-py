a98ff60dfe9eae51862f2de6f18f3335694c036f
Flynn <wphestiraid@gmail.com>
2023-09-07 10:45:16 +0900

    solution: 연속 펄스 부분 수열의 합

--- solutions/level_3/sum_of_a_sequence_of_continuous_pulse_parts/__init__.py
+++ solutions/level_3/sum_of_a_sequence_of_continuous_pulse_parts/__init__.py
@@ -1,3 +1,8 @@
-def solution(sequence):
-    answer = 0
-    return answer
+def solution(sequence: list[int]):
+    prefix_sum = [0]
+
+    for index in range(len(sequence)):
+        pulse = -1 if index % 2 else 1
+        prefix_sum.append(prefix_sum[-1] + pulse * sequence[index])
+
+    return abs(max(prefix_sum) - min(prefix_sum))
