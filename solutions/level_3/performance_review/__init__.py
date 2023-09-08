5b826fc550cdd4f6f0d5a7b627d862042e28ee2a
Flynn <wphestiraid@gmail.com>
2023-09-08 09:20:54 +0900

    solution: 인사고과

--- solutions/level_3/performance_review/__init__.py
+++ solutions/level_3/performance_review/__init__.py
@@ -1,3 +1,18 @@
 def solution(scores):
-    answer = 0
-    return answer
+    result = 1
+    target = scores[0]
+    target_score = sum(target)
+
+    scores.sort(key=lambda x: (-x[0], x[1]))
+
+    prev_ref = 0
+    for score in scores:
+        if target[0] < score[0] and target[1] < score[1]:
+            return -1
+
+        if prev_ref <= score[1]:
+            if target_score < sum(score):
+                result += 1
+            prev_ref = score[1]
+
+    return result
