bd90e81d77a4fe1c06389a660fa70c6db643185b
Flynn <wphestiraid@gmail.com>
2023-09-10 16:54:31 +0900

    solution: 표현 가능한 이진트리

--- solutions/level_3/a_binary_tree_that_can_be_represented/__init__.py
+++ solutions/level_3/a_binary_tree_that_can_be_represented/__init__.py
@@ -1,3 +1,33 @@
+import math
+
+
+def check_parent(number, parent):
+    if number == '':
+        return True
+
+    mid = len(number) // 2
+    current = number[mid]
+
+    if current == '1' and parent == '0':
+        return False
+    return check_parent(number[:mid], current) and check_parent(number[mid + 1 :], current)
+
+
+def check_number(number):
+    if number == 1:
+        return 1
+
+    binary_number = bin(number)[2:]
+    digit = 2 ** (int(math.log(len(binary_number), 2)) + 1) - 1
+    binary_number = '0' * (digit - len(binary_number)) + binary_number
+
+    if binary_number[len(binary_number) // 2] == '1' and check_parent(binary_number, True):
+        return 1
+    return 0
+
+
 def solution(numbers):
-    answer = []
-    return answer
+    result = []
+    for number in numbers:
+        result.append(check_number(number))
+    return result
